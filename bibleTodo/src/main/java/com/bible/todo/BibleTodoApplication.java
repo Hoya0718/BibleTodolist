7366e59a1925f41880c6ddfaefca7b33a6b99c05
TakHoya <130663193+Hoya0718@users.noreply.github.com>
2024-10-25 00:21:56 +0900

    commit

---
+++ bibleTodo/src/main/java/com/bible/todo/BibleTodoApplication.java
@@ -0,0 +1,13 @@
+package com.bible.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleTodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleTodoApplication.class, args);
+	}
+
+}
---
+++ bibleTodo/src/test/java/com/bible/todo/BibleTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bible.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleTodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
